54584a1f4099e4ae9943233d8a0703541f894bef
Fábio <121135484+Fabi0290@users.noreply.github.com>
2024-11-13 23:17:01 +0000

    2 commit

--- app/src/main/java/com/example/trab/policies.kt
+++ app/src/main/java/com/example/trab/policies.kt
@@ -1,36 +1,23 @@
 package com.example.trab
 
-import android.content.Intent
 import android.os.Bundle
 import android.text.Html
-import android.view.View
-import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class policies : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_policies)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_policies)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun login(view: View) {
-        val intent= Intent(applicationContext, LoginActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun termos(view: View) {
-
-        val intent= Intent(applicationContext, policies::class.java)
-        startActivity(intent)
     }
 }
