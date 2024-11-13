46452f2a6a39e459474e226ef5e86cbd0c948a6b
Fábio <121135484+Fabi0290@users.noreply.github.com>
2024-11-13 22:49:14 +0000

    2 commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.trab"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.trab"
--- app/src/main/java/com/example/trab/LoginActivity.kt
+++ app/src/main/java/com/example/trab/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.trab
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,9 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun inicio(view: View) {
+        val intent= Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
     }
 }
